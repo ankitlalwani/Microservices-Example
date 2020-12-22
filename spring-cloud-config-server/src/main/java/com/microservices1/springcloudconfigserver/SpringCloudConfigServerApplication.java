969beea6f577fb34d46b88c5fe4085baff6fac06
Ankit Lalwani <36794216+ankitlalwani@users.noreply.github.com>
2020-12-21 22:13:22 -0500

    config server

---
+++ spring-cloud-config-server/src/main/java/com/microservices1/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -0,0 +1,17 @@
+package com.microservices1.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+
+}
